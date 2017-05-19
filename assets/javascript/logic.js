e957fb1f2d371d00eb78c894083e6c8a5041af27
iamDio <claudio.villani00@gmail.com>
2017-05-19 00:21:50 -0400

    Fixed double object bug

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,56 +1,52 @@
-//$(document).ready(function() {
-
-
+$(document).ready(function() {
 
     //_________VARIABLES__________
 
-    /*var gifDump = $('gifDump');*/
+
     var topics = ['Soccer', 'American Football', 'Baseball', 'Basketball', 'Hockey'];
 
     //_________FUNCTIONS__________
 
-
     function generateGifInfo() {
 
         var sport = $(this).attr('data-name');
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +sport + 
-        "&api_key=dc6zaTOxFJmzC&limit=10" ;
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + sport +
+            "&api_key=dc6zaTOxFJmzC&limit=10";
 
-              $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).done(function(response) {
-            		console.log(response)
-                //var response = response.data;
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).done(function(response) {
+            console.log(response)
 
-               // for (var i=0; i<response.length; i++){}
-/*
-                //create gifDiv element with a class of gifDump to hold the gifs
-                var gifDiv = $("<div>");*/
 
-                //grabs image url from ajax request
-                var imageUrl = response.data[0].images.fixed_height.url;
+        var response = response.data;
+        for (var i=0; i<response.length; i++){
 
-                //creates image tag used to display image
-                var gifElement = $('<img>');
+            //grabs image url from ajax request
+            var imageUrl = response[i].images.fixed_height.url;
 
-                //attaches image url src to created img tag
-                gifElement.attr('src', imageUrl);
-                gifElement.attr('alt', 'sport image');
+            //creates image tag used to display image
+            var gifElement = $('<img>');
 
-                $('.gifDump').append(gifElement);
-				
-            })
-   
+            //attaches image url src to created img tag
+            gifElement.attr('src', imageUrl);
+            gifElement.attr('alt', 'sport image');
+
+            $('.gifDump').append(gifElement);
+                }
+        })
+
     }
-//displaying gifs when clicked   
-$(document).on('click','.sportButton', generateGifInfo);
+
+    //displaying gifs when clicked   
+    $(document).on('click', '.sportButton', generateGifInfo);
+   
 
 
 
     //iterates through the array of given sports
-    function generateButton() {	
-    	
+    function generateButton() {
         $("#buttonsDump").empty();
 
         topics.forEach(function(category) {
@@ -65,27 +61,34 @@ $(document).on('click','.sportButton', generateGifInfo);
             $("#buttonsDump").append(buttons);
         });
     }
- 	 
-
-//});
 
 //generates pre determined buttons
-generateButton();
+    generateButton();
 
-/*
-	 $('.sportButton').on("click", function(event) { 
-    	event.preventDefault();
+    
+     $('.sportButton').on("click", function(event) { 
+        event.preventDefault();
 
-    	var sportGif = $('#gifInput').val().trim();
+        var sportGif = $('#gifInput').val().trim();
 
-    	topics.push(sportGif);
+        topics.push(sportGif);
 
-    	    
+            
 
-    });*/
+    });
+});
+
+
+
+
+
 
 //__________________________________________-
 /*
 
         //for (var i = 0; i<topics.length; i++){}
-	console.log(category);*/
+    console.log(category);*/
+
+
+//create gifDiv element with a class of gifDump to hold the gifs
+// var gifDiv = $("<div>");
